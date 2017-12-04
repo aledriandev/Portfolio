a515cfff30f58382524a4cbfd1b713357403298f
unknown <alejandraadriantejda@gmail.com>
2017-12-04 12:17:47 -0500

    home responsive

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ import me from '../images/ale/ale1.gif';
 const About = ({ ale }) => {
   const pins = ale.badges.map((pin,index)=>{
     return( 
-      <Col md={4}>
+      <Col md={4} sm={4} xs={4}>
         <div className='back-pin'>
           <img className='pin' src={pin}/>
           <div className='span-pin'><b>{ale.badgesD[index]}</b></div>
@@ -15,7 +15,7 @@ const About = ({ ale }) => {
   return (
     <div className='about-me form-v'>
       <Row className='content-about'>
-        <Col mdOffset={1} md={6} smOffset={2} sm={5} xs={12}>
+        <Col mdOffset={1} md={6} smOffset={1} sm={10} xsOffset={1} xs={10}>
           <div className='my-bio'>
             <div className='title'>
               <h1 className='title-parts'>SOBRE MÍ</h1>
@@ -26,7 +26,7 @@ const About = ({ ale }) => {
             <div className='pins'>{pins}</div>
           </div>
         </Col>
-        <Col md={4} sm={5} xs={12}>
+        <Col md={4} sm={12} xs={12}>
           <div className='photo-me'>
             <img className='two' src={me} />
           </div>
